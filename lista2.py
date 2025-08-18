bd50c834fe53898c4764ba10f522f0f9906b7379
Gab7Barbosa <gabriel.silva1521@aluno.ce.gov.br>
2025-08-18 12:00:47 -0300

    lista2.py

---
+++ lista2.py
@@ -0,0 +1,27 @@
+#soma = 0
+#for i in range(3):
+#    notas = int(input("Digite suas notas"))
+#    soma += notas 
+#media = soma/3
+#print(f"sua media é {media}")
+#if media >= 7:
+#    print("Você está aprovado")
+#else: 
+#    print("Você está reprovado")
+
+
+#cont = 0 
+#pedir_palavra = input("Digite uma palavra ").lower()
+#vogais = ["a","e","i","o","u"]
+#for i in pedir_palavra:
+ #   if i in vogais:
+  #      cont +=1
+#print(cont) 
+
+
+#import math
+  #num = int(input("Digite um numero:"))
+#fatorial = math.factorial(num)
+#print(fatorial) 
+
+
