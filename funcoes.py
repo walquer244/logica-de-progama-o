13ec1b988efa1f56e0a96995cd77567c86e6ddc3
walquer244 <isaac.sousa77@aluno.ce.gov.br>
2025-09-18 09:02:21 -0300

    funcoes

---
+++ funcoes.py
@@ -0,0 +1,97 @@
+# def mensagem(nome):
+#     return(f"Olá {nome}!, seja bem vindo(a) a aula de logica")
+# print(mensagem(input("Digite seu nome: ")))
+
+# def numero(n):
+#     if n % 2 ==0:
+#         return(f"seu numero é par")
+#     else:
+#         return(f"seu numero é impar")
+# print(numero(int(input("digite seu numero"))))
+
+
+#feito
+# n1 = int(input("Digite um numero: "))
+# n2 = int(input("Digite outro numero: "))
+# if n1 > n2:
+#     print(f"{n1} é maior que {n2}")
+# elif n1 < n2:
+#     print(f"{n2} é maior que {n1}")
+# else:
+#     print("Eles são iguais")     
+
+# numero=int(input("Digite um numero:"))
+# for i in range(1,11):
+#     print(f"{numero} x {i} = {numero*i}")
+
+# for i in range(10,0, -1):
+#     print(i)
+# print("booom")    
+
+# def notas(w, x, y, z):
+#     media = (w+x+y+z)/4
+#     return(media)
+# print(notas(10.0, 9.0, 8.0, 10.0))
+
+#feito
+# import math
+# def fatorial(x):
+#     fatorial = math.factorial(x)
+#     return(f"{fatorial} é o resultado da operação")
+# print(fatorial(int(input("Digite um numero: "))))  
+
+#precisa ser feito
+# contavogais = 0
+# vogais=["a", "e", "i", "o", "u"]
+# def letra(palavra):
+#         palavra = input("digite uma palavra")
+#         return(f"A palavra que voce digitou tem {contavogais} vogais")
+# if vogais in letra:
+#         if letra in vogais:
+#         contavogais += 1
+# else:
+#         contavogais == 0        
+
+    
+# print(letra(input("Digite uma palavra: ")))              
+
+
+#feito
+# import random
+# opcoes = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+# pc_choice = random.choice(opcoes)
+# while True:
+#     user_choice = int(input("tente adivinhar meu numero: "))
+#     if user_choice < pc_choice:
+#         print("Meu numero é maior!")
+#     elif user_choice > pc_choice:
+#         print("meu numero é menor")
+#     else:
+#         print("Você acertou")
+#         break
+
+
+#precisa ser feito
+# pares = [0,2,4,6,8]
+# def soma(a):
+#     numero = int(input("Digite um numero: "))
+#     for i in pares:
+#       if pares in numero:
+#          return(f"a soma dos numeros pares é {soma}")  
+# print(soma(int(input("Digite um numero: "))))
+
+
+#14(feito)
+# contapar = 0
+# contaimpar = 0
+# for i in range(3):
+#     numeros = int(input("Digite numeros diferentes: "))
+#     if numeros % 2 == 0:
+#         contapar += 1
+#     elif numeros != 0:
+#         contaimpar += 1
+# print(contaimpar)            
+# print(contapar)
+
+
+
